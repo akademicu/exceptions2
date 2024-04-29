b5b154a4b83d83f9f1144b5c665fdf52644798e7
IOn <akademicu@mai.ru>
2024-04-29 16:59:00 +0100

    Exercise3

---
+++ src/main/java/exceptions/Exercise3.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class Exercise3 {
+
+    public static void divide() {
+
+        int[] inputs;
+
+        inputs = Exercise2.getUserInput();
+        System.out.println(Exercise1.divide(inputs[0],inputs[1]));
+
+
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,16 @@ package org.example;
 
 import exceptions.Exercise1;
 import exceptions.Exercise2;
+import exceptions.Exercise3;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        int[] inputs;
+        /*int[] inputs;
         inputs = Exercise2.getUserInput();
-        System.out.println(Exercise1.divide(inputs[0], inputs[1]));
+        System.out.println(Exercise1.divide(inputs[0], inputs[1]));*/
+
+        Exercise3.divide();
     }
 }
