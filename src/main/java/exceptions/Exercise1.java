4487ea0096c091a5001438687eee023cf7015167
IOn <akademicu@mai.ru>
2024-04-29 16:40:25 +0100

    Exercise1

---
+++ src/main/java/exceptions/Exercise1.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class Exercise1 {
+    public static  int divide(int nr1, int nr2){
+        return nr1/nr2;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import exceptions.Exercise1;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        System.out.println(Exercise1.divide(10, 2));
+        System.out.println(Exercise1.divide(10, 0));
     }
 }
